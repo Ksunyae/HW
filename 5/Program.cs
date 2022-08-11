be0acd96b6e6ef2fb65160ccf7197b0ee19d4c78
Kseniya <keusy@yandex.ru>
2022-08-11 15:27:06 +0300

    add 5

---
+++ 5/Program.cs
@@ -0,0 +1,10 @@
+﻿int [] array = new int[4];
+int count = 0;
+for (int i = 0; i < 4; i++)
+    array[i] = new Random().Next(0, 100);
+
+Console.WriteLine("[" + string.Join(",", array) + "]");   
+
+int sum = 0;
+sum = sum + array[1] + array[3];
+Console.WriteLine(sum);
