ffd769575620920275a347836f41cb369adc0a79
Kseniya <keusy@yandex.ru>
2022-08-11 15:27:28 +0300

    add 6

---
+++ 6/Program.cs
@@ -0,0 +1,24 @@
+﻿int [] array = new int[4];
+int min = 0;
+int max = 0;
+int result = 0;
+for (int i = 0; i < 4; i++)
+    array[i] = new Random().Next(0, 100);
+
+Console.WriteLine("[" + string.Join(",", array) + "]");   
+
+for (int i = 0; i < 4; i++);
+{
+    if (array[i] < min);
+    min = array[i];
+}
+
+for (int i = 0; i < 4; i++);
+{
+    if (array[i] > max);
+    max = array[i];
+}
+
+result = result + max - min;
+Console.WriteLine(result);
+
