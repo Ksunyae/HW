6f65f4fddf247d7b54da09ac435cc8a1a2bbcd9a
Kseniya <keusy@yandex.ru>
2022-08-11 15:26:43 +0300

    add 4

---
+++ 4/Program.cs
@@ -0,0 +1,9 @@
+﻿int [] array = new int[4];
+int count = 0;
+for (int i = 0; i < 4; i++)
+    array[i] = new Random().Next(100, 1000);
+
+Console.WriteLine("[" + string.Join(",", array) + "]");   
+
+var result = array.Count(x => x % 2 == 0);
+Console.WriteLine(result);
