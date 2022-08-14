0d8e8e64d3fab3dd96d1ffeba2ebee85ef435939
Kseniya <keusy@yandex.ru>
2022-08-14 20:02:21 +0300

    add 8, 9, 10

---
+++ 10/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[,] matrix = new int[2, 2];
+for (int i = 0; i < 2; i ++)
+{
+    for (int j = 0; j< 2; j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+    }
+    
+}
+for (int i =0; i<2; i++)
+{
+    for (int j = 0; j< 2; j++)
+        Console.Write(matrix[i, j]+ "\t");
+    Console.WriteLine();
+}
+
+
+int a = ((matrix[0, 0] + matrix[0, 1]) / 2);
+int b = ((matrix[1, 0] + matrix[1, 0]) / 2);
+Console.WriteLine(string.Join (",", a, b));
+
---
+++ 9/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+for (int i = 0; i < n; i ++)
+{
+    for (int j = 0; j< m; j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+    }
+    
+}
+for (int i =0; i<n; i++)
+{
+    for (int j = 0; j< m; j++)
+        Console.Write(matrix[i, j]+ "\t");
+    Console.WriteLine();
+}
+Console.Write("Введите любую цыфру: ");
+int s = Convert.ToInt32(Console.ReadLine());
+
+for (int i =0; i<n; i++)
+{
+    for (int j = 0; j< m; j++)
+    {
+        if (matrix[i, j] == s)
+           Console.WriteLine(s);
+        else
+           Console.WriteLine("Такого числа в массиве нет");
+    }
+}
+
+
+
