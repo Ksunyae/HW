5bd2032509fe515007ef6565f0c4ec7f23ab9c12
Kseniya <keusy@yandex.ru>
2022-08-30 11:56:34 +0300

    add 17, 18

---
+++ 17/Program.cs
@@ -0,0 +1,21 @@
+﻿66
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите значение M: ");
+            uint M = Convert.ToUInt32(Console.ReadLine());
+        
+            Console.Write("Введите значение N: ");
+            uint N = Convert.ToUInt32(Console.ReadLine());
+            uint sum = 0;  
+            for (uint i = M; i <= N; i++)  
+                sum += i; 
+            Console.WriteLine($"Сумма чисел в промежутке от {M} до {N} = {sum}"
+            Console.ReadKey(); 
+        }
+    }
+}
+
---
+++ 18/Program.cs
@@ -0,0 +1,29 @@
+﻿68
+{
+    internal class Program
+    {
+    
+        static uint Akkerman(uint m, uint n)
+        {
+            if (m == 0)  
+                return n + 1;
+            else
+              if ((m != 0) && (n == 0))
+                return Akkerman(m - 1, 1);  
+            else
+                return Akkerman(m - 1, Akkerman(m, n - 1));  
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение m: ");
+            uint m = Convert.ToUInt32(Console.ReadLine()); 
+            Console.Write("Введите значение n: ");  
+            uint n = Convert.ToUInt32(Console.ReadLine()); 
+            uint A = Akkerman(m, n); 
+            Console.WriteLine($"A({m},{n}) = {A}"); 
+            Console.ReadKey(); 
+        }
+    }
+}
+
